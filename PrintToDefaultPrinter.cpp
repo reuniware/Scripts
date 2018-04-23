d8c905df0777850c03b2db6af674576a1b604e4b
Fintech Opensource Projects <reunisoft@gmail.com>
2018-04-23 18:04:39 +0200

    Update PrintToDefaultPrinter.cpp

--- PrintToDefaultPrinter.cpp
+++ PrintToDefaultPrinter.cpp
@@ -1,4 +1,4 @@
-// GDITest.cpp : définit le point d'entrée pour l'application console.
+// PrintToDefaultPrinter.cpp : définit le point d'entrée pour l'application console.
 //
 
 #include "stdafx.h"
